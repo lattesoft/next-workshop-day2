da8b2c45ee5eed34f438d0b219b729a252ed7021
Komphet Meesab <komphet.me@gmail.com>
2020-02-02 13:07:19 +0700

    Update HOC

--- 3-create-dynamic-pages/pages/post.js
+++ 3-create-dynamic-pages/pages/post.js
@@ -1,3 +1,15 @@
-export default ()=>{
-    return <div>This is a post page</div>
+import { useRouter } from 'next/router'
+import Layout from '../components/MyLayout.js'
+
+const Page = () => {
+  const router = useRouter()
+
+  return (
+    <Layout>
+      <h1>{router.query.title}</h1>
+      <p>This is the blog post content.</p>
+    </Layout>
+  )
 }
+
+export default Page
