60d89a0d7323ead8c1360d0975413eceb6965484
Komphet Meesab <komphet.me@gmail.com>
2020-02-02 16:46:50 +0700

    Update

--- 6-fetching-data/pages/_app.js
+++ 6-fetching-data/pages/_app.js
@@ -1,8 +1,12 @@
 import Layout from "../components/MyLayout"
+import store from '../redux/store'
+import {Provider} from 'react-redux'
+import App, {Container} from 'next/app'
+import withRedux from 'next-redux-wrapper'
+import compose from 'recompose/compose';
 
 class MyApp extends App {
     render() {
-    
         return (
             <Container>
                 <Provider store={store}>
@@ -13,4 +17,6 @@ class MyApp extends App {
     }
 }
 
-export default MyApp;
+export default compose(
+    withRedux(initStore),
+)(MyApp)
