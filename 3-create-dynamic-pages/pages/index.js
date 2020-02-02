f34b23cb09021e98b0245fa5a44c50093237a260
Komphet Meesab <komphet.me@gmail.com>
2020-02-02 11:56:56 +0700

    Update HOC

--- 3-create-dynamic-pages/pages/index.js
+++ 3-create-dynamic-pages/pages/index.js
@@ -1,9 +1,29 @@
 import Layout from '../components/MyLayout.js'
 
+const posts = [
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing",
+    content: "Hellooooooooo"
+  },
+  {
+    title: "ever since the 1500s, when an unknown printer took",
+    content: "Hellooooooooo"
+  },
+  {
+    title: "remaining essentially unchanged",
+    content: "Hellooooooooo"
+  }
+];
+
 export default function Index() {
   return (
     <Layout>
       <p>Hello Next.js</p>
+      <ol>
+      {posts.map((post,index)=>{
+      return <li>{post.title}</li>
+      })}
+      </ol>
     </Layout>
   )
 }
